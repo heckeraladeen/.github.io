8f2da3272325b1f2767ae3cbe7be4745369916c8
Yurie Russel G Priolo <149810481+YurieRussel@users.noreply.github.com>
2026-07-05 00:18:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ const gravity = 0.7;
 
 const hitSFX_player1 = new Audio('snd-hammer-attacks.mp3');
 //const hitSFx_player2 = new Audio();
+const hitSFX_player2 = new Audio('');
 
-hitSFX_player1.play();
+
 
 
 
